19c7677dfd15f371f9548cd26ee6dacdf1c51051
Sergej Rube <rube@codeunity.de>
2021-09-16 15:07:17 +0200

    changed signIn Policy

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
     auth: {
         clientId: "e35cbc3c-7055-43a2-b978-d8d24a040140",
-        authority: "https://login.mytapio.one/32896ed7-d559-401b-85cf-167143d61be0/B2C_1A_TAPIO_SIGNIN_SHORT/", // https://<your-tenant>.b2clogin.com/<your-tenant>.onmicrosoft.com/<your-policyID>
+        authority: "https://login.mytapio.one/32896ed7-d559-401b-85cf-167143d61be0/B2C_1A_TAPIO_SIGNIN/", // https://<your-tenant>.b2clogin.com/<your-tenant>.onmicrosoft.com/<your-policyID>
         redirectUri: "https://tapioone.github.io/tapio-auth-react/", // for dev use http://localhost:3000
         knownAuthorities: ["https://login.mytapio.one"]
     },
